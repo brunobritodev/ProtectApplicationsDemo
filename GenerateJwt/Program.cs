c06d95d81b39f87126c92df67271ddcc57e21110
Bruno Brito <bhdebrito@gmail.com>
2021-01-14 12:23:38 -0300

    Separando validacoes

--- GenerateJwt/Program.cs
+++ GenerateJwt/Program.cs
@@ -19,16 +19,6 @@ namespace TestJwt
             Console.WriteLine($"{jws}{Environment.NewLine}");
             Console.WriteLine("Copie o JWS no jwt.io");
             Console.ReadKey();
-
-            /*
-             * PARTE 2 - Validar JWS
-             */
-            // var jws = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjZmMzZmYTczLTE4ZmMtNDBmMC1iNGU1LWJmNDhmZDU5ZmQxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1ldWVtYWlsQGdtYWlsLmNvbSIsImdpdmVuX25hbWUiOiJCcnVubyBCcml0byIsInN1YiI6ImMwMTI1MWE3LTE3YjItNGI0Ny04ODU3LTY1OWFiZDUzMzczMiIsImF1ZCI6InlvdXItc3BhIiwiZXhwIjoxNjEwNjQwNDU2LCJpc3MiOiJ3d3cubXlzaXRlLmNvbSIsImlhdCI6MTYxMDYzNjg1NiwibmJmIjoxNjEwNjM2ODU2fQ.np0fWhC2-mAvRvcrjPkAsS5yMGV2P0SB96NeWbc19ReWxBrBzwVxNYUKHI4BVsaHkHKy0m0DcO_86O5uw8Jsrlhx3xJWPnSx2qrvp_3ctgHlwol1eMnCbJ3MqgW4A48uM_4l7o_8OUvyjpR3pIEs9xjbixm7O-1IlMrvdmVrqomUngS1vVSfMD1ZgV2JyYCZV6sA77l4dYnlb-mkZHD8sLm03mshMIMYctAVg6NqXasSIiTA6m5lGtYWhfGbY8C6bmG8QR4wBHIabytVVIAe0bZJ0lBpH4fm0teBhZ47vTkMN1hBZPWKwu_22tDMNkNlIZwAhjrLO6q2_kuwHP4yKg";
-            // 
-            //var validationResult = RsaExample.Validate(jws);
-            //Console.WriteLine("Este JWT é válido ?: {0}", validationResult.IsValid);
-            //Console.WriteLine("Claims:");
-            //Console.WriteLine(JsonSerializer.Serialize(validationResult.ClaimsIdentity.Claims.Select(s => new { s.Type, s.Value }), new JsonSerializerOptions() { IgnoreNullValues = true, WriteIndented = true }));
         }
 
         private static string GenerateToken()
---
+++ ValidateJwt/RsaExample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ValidateJwt
+{
+    public class RsaExample
+    {
+        private static readonly string PublicJwkFile = Path.Combine(GetApplicationRoot(), "public-tempkey.jwk");
+     
+        public static TokenValidationResult Validate(string jwt)
+        {
+            var tokenValidationParams = new TokenValidationParameters
+            {
+                ValidIssuer = "www.mysite.com",
+                ValidAudience = "your-spa",
+            };
+
+            var tokenHandler = new JsonWebTokenHandler();
+            var storedJwk = JsonSerializer.Deserialize<PublicJsonWebKey>(File.ReadAllText(PublicJwkFile));
+            tokenValidationParams.IssuerSigningKey = storedJwk.ToNativeJwk();
+            var validationResult = tokenHandler.ValidateToken(jwt, tokenValidationParams);
+
+            return validationResult;
+        }
+
+        public static void SavePublicJwk(JsonWebKey newJwk)
+        {
+            var pub = new PublicJsonWebKey(newJwk);
+            File.WriteAllText(PublicJwkFile, JsonSerializer.Serialize(pub, new JsonSerializerOptions() { IgnoreNullValues = true, WriteIndented = true }));
+        }
+
+        private static string GetApplicationRoot()
+        {
+            var exePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            Regex appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+            var appRoot = appPathMatcher.Match(exePath).Value;
+            var dir = Path.Combine(Directory.GetParent(appRoot).FullName, "GenerateJwt");
+            return dir;
+        }
+    }
+}
